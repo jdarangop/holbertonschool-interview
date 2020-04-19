4abbd31447ae2e7e2984f78ce5c81d8031c9f9ad
jdarangop <juandiegoap33@gmail.com>
2020-04-19 06:59:27 +0000

    Convert some edge cases in task 0 of lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,7 +8,7 @@ def inspectboxes(box, boxes, keys):
         keys: set
     """
     for i in box:
-        if i not in keys:
+        if i not in keys and i < len(boxes):
             keys.add(i)
             inspectboxes(boxes[i], boxes, keys)
 
@@ -17,6 +17,8 @@ def canUnlockAll(boxes):
     """ Find if all the boxes can be opened
         boxes: list of list
     """
+    if boxes == [] or boxes is None:
+        return False
     keys = set([0])
     inspectboxes(boxes[0], boxes, keys)
 
