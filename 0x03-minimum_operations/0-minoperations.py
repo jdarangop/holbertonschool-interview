e9128cd09c3baf778674b58d0a83f39173c800b5
jdarangop <juandiegoap33@gmail.com>
2020-05-13 01:10:44 +0000

    Change None for 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,8 +8,8 @@ def minOperations(n):
         n: int Number of elements in a String.
         Return: Minimum number of this operations.
     """
-    if type(n) != int or n <= 0:
-        return None
+    if type(n) != int or n <= 1:
+        return 0
     result = 0
     for i in range(2, n + 1):
         while(n % i == 0):
