a8e70167f8606ef9e7f59d1a0912cb400ae4b24c
jdarangop <juandiegoap33@gmail.com>
2020-05-26 22:59:22 +0000

    Add the folder 0x05-linked_list_palindrome with the task

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+
+/**
+ * len - find the lenght of the linked list.
+ * @head: head of the linked list.
+ * Return: The lenght of the linked list.
+ */
+int len(listint_t *head)
+{
+	int counter = 0;
+
+	for (; head != NULL; head = head->next)
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * is_palindrome - check if a linked list is palindrome.
+ * @head: head of the linked list.
+ * Return: 1 if it is palindrome, 0 if doesn't
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	int i, j, lenght, *numbers;
+
+	lenght = len(*head);
+	numbers = malloc(sizeof(int) * lenght);
+	for (i = 0; i < lenght; i++)
+	{
+		numbers[i] = current->n;
+		current = current->next;
+	}
+	for (i = 0, j = lenght - 1; i < j; i++, j--)
+	{
+		if (numbers[i] != numbers[j])
+		{
+			free(numbers);
+			return (0);
+		}
+	}
+	free(numbers);
+	return (1);
+}
