d8a9641ad8c74f352f2eaf87b4123b4f70b2a0ea
jdarangop <juandiegoap33@gmail.com>
2020-11-11 13:35:15 -0500

    Add the folder 0x19-making_change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Make Change """
+
+
+def makeChange(coins, total):
+    """ determine the fewest number of coins
+        needed to meet a given amount total.
+        Args:
+            coins: (list) the values of the coins in your possession.
+            total: (int) amount to be calculated.
+        Returns:
+            (int) fewest number of coins needed to meet total
+                  or -1.
+    """
+    if total <= 0:
+        return 0
+    coins = sorted(coins)
+    change = [total + 1] * (total + 1)
+    change[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin <= i:
+                change[i] = min(change[i], 1 + change[i - coin])
+
+    return change[total] if change[total] < total else -1
