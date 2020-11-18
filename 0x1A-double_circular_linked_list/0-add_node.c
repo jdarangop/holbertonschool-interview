94f74bbe8778ba63c2d0bbcedc3b4e12ac70b1a0
jdarangop <juandiegoap33@gmail.com>
2020-11-18 12:25:54 -0500

    Add the folder 0x1A-double_circular_linked_list

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,83 @@
+#include "list.h"
+
+/**
+ * add_node_end - add a node on the end.
+ * @list: Double linked list.
+ * @str: string value in the new node.
+ *
+ * Return: Memory addres to the new node.
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *current, *new_node;
+
+	if (list == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	current = *list;
+	if (*list == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*list = new_node;
+	}
+	else
+	{
+		while (current->next != NULL && current->next != *list)
+		{
+			current = current->next;
+		}
+		new_node->prev = current;
+		current->next = new_node;
+		new_node->next = *list;
+		(*list)->prev = new_node;
+	}
+	return (new_node);
+}
+
+/**
+ * add_node_begin - add a node on the beginning.
+ * @list: Double linked list.
+ * @str: string value in the new node.
+ *
+ * Return: Memory addres to the new node.
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *current, *new_node;
+
+	if (list == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (*list == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
+	else
+	{
+		current = *list;
+		while (current->next != NULL && current->next != *list)
+		{
+			current = current->next;
+		}
+		if (current->prev == NULL)
+			current->prev = new_node;
+		new_node->prev = current;
+		new_node->next = *list;
+		(*list)->prev = new_node;
+		current->next = new_node;
+	}
+	*list = new_node;
+
+	return (new_node);
+}
