32d192a7157ebfb678d6883fbb625c7fab489eb6
jdarangop <juandiegoap33@gmail.com>
2020-11-25 15:02:01 -0500

    Add the folder 0x1B-radix_sort

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void radix_count(int *array, int size, int j);
+
+#endif
