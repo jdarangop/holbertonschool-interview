0c61a93ee4413e916bc6bbddd3be8a32d3e4f7e6
jdarangop <juandiegoap33@gmail.com>
2021-01-02 13:56:29 -0500

    Add the project 0x1F-pascal_triangle

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bine/env python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing
+        the Pascal’s triangle of n.
+        Args:
+            n: (int) exponent.
+        Returns:
+            (list of lists) of integers representing
+            the Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = []
+            prev = triangle[i - 1]
+            for j in range(len(prev) + 1):
+                if j == 0 or j == len(prev):
+                    if j == 0:
+                        row.append(1)
+                    if j == len(prev):
+                        row.append(1)
+                else:
+                    row.append(prev[j - 1] + prev[j])
+            triangle.append(row)
+
+    return triangle
